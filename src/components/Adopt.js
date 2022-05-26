b9205c7e0e69c6649873fe8b614cdec41389e118
unknown <raulheri43@hotmail.com>
2022-05-26 11:58:31 -0300

    correção no adopt

--- src/components/Adopt.js
+++ src/components/Adopt.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import {FaHeart} from "react-icons/fa"
-import Button from "./Button";
 import "../styles/adopt.css"
 import {api} from "./api";
 
