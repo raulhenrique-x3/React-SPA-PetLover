5d3d217869e5c9f5c5b79aac1931bdc6a9a77db4
unknown <raulheri43@hotmail.com>
2022-05-26 11:43:38 -0300

    atualização de arquivos

--- src/components/Button.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-    function Button(){
-        return(
-            <button type="submit" name="submit" className="subButton">Acessar!</button>
-        )
-    }
-    
-export default Button;
--- src/components/Input.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import "../styles/input.css"
-
-    function Input(){
-        return(
-            <input required/>
-        )
-    }
-export default Input;
